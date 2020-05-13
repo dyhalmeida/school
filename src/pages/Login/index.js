bde9092ec49f6fd9a1267fad753ad4a8d9d6a948
Diego Almeida <dyhalmeida@gmail.com>
2020-05-13 11:51:32 -0300

    Adicionado estilo global com styled-components

---
+++ src/pages/Login/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Title } from './styled';
+
+function Login() {
+  return (
+    <>
+      <Title>Login Page</Title>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus,
+        nobis. Suscipit esse eius vel, sit minima accusamus unde iure quo
+        consequatur commodi reiciendis veritatis nihil et explicabo expedita
+        distinctio officia?
+      </p>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/pages/Login/styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 1.2rem;
+  color: #ff0000;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+`;

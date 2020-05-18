a397f2471ea9a37af1303916540fd765123b0e86
Diego Almeida <dyhalmeida@gmail.com>
2020-05-18 15:09:14 -0300

    Adicionado componente para criar conta de usuário

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,10 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { get } from 'lodash';
+import axios from '../../services/axios';
+import history from '../../services/history';
+
 import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    let error = false;
+
+    if (name.length <= 3 || name.length > 30) {
+      error = true;
+      toast.error('Nome deve ter entre 3 e 30 caracteres');
+      return;
+    }
+
+    if (!isEmail(email)) {
+      error = true;
+      toast.error('E-mail inválido');
+    }
+
+    if (password.length <= 3 || password.length > 8) {
+      error = true;
+      toast.error('Senha deve ter entre 3 e 10 caracteres');
+      return;
+    }
+
+    if (error) return;
+
+    try {
+      await axios.post('/users', {
+        name,
+        email,
+        password,
+      });
+      toast.success('Conta criada com sucesso');
+
+      /** Roteia para página de login */
+      history.push('/login');
+    } catch (err) {
+      // const status = get(err, 'response.status', 0);
+      const errors = get(err, 'response.data.errors', []);
+      errors.forEach((errorText) => toast.error(errorText));
+    }
+  }
+
   return (
     <Container>
-      <h1>Register Page</h1>
+      <h1>Crie sua conta</h1>
+      <Form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Nome:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            placeholder="Seu nome"
+          />
+        </label>
+        <label htmlFor="email">
+          E-mail:
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            placeholder="Seu e-mail"
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            placeholder="Sua senha"
+          />
+        </label>
+        <button type="submit">Criar minha conta</button>
+      </Form>
     </Container>
   );
 }
--- src/pages/Register/styled.js
+++ src/pages/Register/styled.js
@@ -1,3 +1,26 @@
 import styled from 'styled-components';
+import Colors from '../../config/colors';
 
-export const Title = styled.h1``;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+  input {
+    margin-top: 5px;
+    height: 32px;
+    padding: 0 10px;
+    border-radius: 4px;
+    border: 1px solid ${Colors.primaryDark};
+    font-size: 15px;
+/*
+    &:focus {
+      border: 1px solid ${Colors.primaryDark};
+    } */
+  }
+`;
